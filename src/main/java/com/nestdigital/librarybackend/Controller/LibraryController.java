df031041463345af4aad144f004c60ea139a8f91
Lakshmi <Lakshmi@NTPLAP-008.chn.nestgroup.net>
2022-10-19 20:28:41 +0530

    pushapi

---
+++ src/main/java/com/nestdigital/librarybackend/Controller/LibraryController.java
@@ -0,0 +1,28 @@
+package com.nestdigital.librarybackend.Controller;
+
+import com.nestdigital.librarybackend.Dao.LibraryDao;
+import com.nestdigital.librarybackend.Model.LibraryModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class LibraryController {
+    @Autowired
+    private LibraryDao dao;
+    @CrossOrigin(origins = "*")
+
+    @PostMapping(path = "/addlibrary",consumes = "application/json",produces = "application/json")
+    public String addlibrary(@RequestBody LibraryModel reciepe){
+        System.out.println(reciepe.toString());
+        dao.save(reciepe);
+        return "{status:'success'}";
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/viewlibrary")
+    public List<LibraryModel>viewlibrary(){
+        return (List<LibraryModel>) dao.findAll();
+    }
+}
